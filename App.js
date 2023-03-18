c43d9af08c29afc7fd827fee4b8c44d37fd45e12
Istiak <istiak@istiak-PASSION-BX-PRO>
2023-03-18 18:04:03 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './config/firebase';
+
+import Chat from './screens/chat';
+import Login from './screens/login';
+import Sign_up from './screens/sign_up';
+import Home from './screens/home';
+import { createContext ,useContext,useState,useEffect} from 'react';
+
+const Stack = createStackNavigator();
+const AuthenticationUserContext = createContext({});
+const AuthenticatedUserProvider = ({children})=>{
+  const [user,setUser] =useState();
+  return(
+    <AuthenticationUserContext.Provider value={{user,setUser}}>
+      {children}
+    </AuthenticationUserContext.Provider>
+  )
+}
+
+function ChatStack(){
+  return(
+    <Stack.Navigator defaultScreenOptions={Home} >
+      <Stack.Screen name="Home" component = {Home}/>
+      <Stack.Screen name="Chat" component = {Chat}/>
+    </Stack.Navigator>
+  )
+}
+
+function AuthStack(){
+  return(
+    <Stack.Navigator defaultScreenOptions={Login} screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Login" component = {Login}/>
+      <Stack.Screen name="Signup" component = {Sign_up}/>
+    </Stack.Navigator>
+  )
+}
+
+function RootNavigator(){
+  const {user, setUser} = useContext(AuthenticationUserContext);
+  const [loading,setLoading] = useState(true);
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth,
+      async authenticatedUser=>{
+        authenticatedUser? setUser(authenticatedUser):setUser(null);
+        setLoading(false);
+      });
+      return () => unsubscribe();
+  },[user]);
+  if(loading){
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  }
+  return(
+    <NavigationContainer>
+      {user ? <ChatStack/> : <AuthStack/>}
+      {/* <ChatStack/> */}
+    </NavigationContainer>
+  ) 
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthenticatedUserProvider>
+      <RootNavigator/>
+    </AuthenticatedUserProvider>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default{
   "expo": {
     "name": "firebasePracticeApp",
     "slug": "firebasePracticeApp",
@@ -25,6 +27,15 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra:{
+      apiKey: "AIzaSyDFAgp5pQpD2RUUHYuadQfr33z01VN-L6I",
+      authDomain: "chatapp-4b37e.firebaseapp.com",
+      projectId: "chatapp-4b37e",
+      storageBucket: "chatapp-4b37e.appspot.com",
+      messagingSenderId: "981379444513",
+      appId: "1:981379444513:web:76f304064ba88df2de1860",
+      measurementId: "G-1ST8947R91"
     }
   }
 }
